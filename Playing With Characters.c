f45b519f7a6371b4bd88014ad4356ea7af3c31ca
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2019-10-18 22:00:48 +0530

    Create Playing With Characters.c

---
+++ Playing With Characters.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    char a;
+    char s1[10000];
+    char s2[10000];
+
+
+    scanf("%c",&a);
+    scanf("%s\n",&s1);
+    scanf("%[^\n]%*c", &s2);
+
+
+    printf("%c",a);
+    printf("\n");
+
+    printf(s1);
+    printf("\n");
+
+    printf(s2);
+    printf("\n");
+
+    return 0;
+}
+

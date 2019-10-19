44b9aae0fc7d23d5ec6bfe13dc03762c83510f08
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2019-10-19 19:47:43 +0530

    Create Conditional Statements in C.c

---
+++ Conditional Statements in C.c
@@ -0,0 +1,36 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+
+
+int main()
+{
+ int n_str;
+ scanf("%d",&n_str);
+    // Write Your Code Here
+ 
+    if(n_str==1){printf("one\n");    }else
+    if(n_str==2){printf("two\n");    }
+    else if(n_str==3){printf("three\n");    }
+     else if(n_str==4){printf("four\n");    }
+    else if(n_str==5){printf("five\n");    }
+    else if(n_str==6){printf("six\n");    }
+    else if(n_str==7){printf("seven\n");    }
+    else if(n_str==8){printf("eight\n");    }
+    else if(n_str==9){printf("nine\n");    }
+    else {
+        printf("Greater than 9\n");
+    }
+
+
+    return 0;
+}

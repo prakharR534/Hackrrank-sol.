6f1b11259fd4d47afa2e452926cfb97a5cf9be85
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2019-10-26 23:48:59 +0530

    Create Tree: height of a tree.cpp

---
+++ Tree: height of a tree.cpp
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+struct node {
+    
+    int data;
+    struct node *left;
+    struct node *right;
+  
+};
+
+struct node* insert( struct node* root, int data ) {
+		
+	if(root == NULL) {
+	
+        struct node* node = (struct node*)malloc(sizeof(struct node));
+
+        node->data = data;
+
+        node->left = NULL;
+        node->right = NULL;
+        return node;
+	  
+	} else {
+      
+		struct node* cur;
+		
+		if(data <= root->data) {
+            cur = insert(root->left, data);
+            root->left = cur;
+		} else {
+            cur = insert(root->right, data);
+            root->right = cur;
+		}
+	
+		return root;
+	}
+}
+
+/* you only have to complete the function given below.  
+node is defined as  
+
+struct node {
+    
+    int data;
+    struct node *left;
+    struct node *right;
+  
+};
+
+*/
+int getHeight(struct node* root) {
+    // Write your code here
+
+    int lh=-1,rh=-1;
+
+    if(root != NULL){
+        if(root->left != NULL){
+            lh = getHeight(root->left);
+
+        }
+        if(root->right != NULL){
+            rh = getHeight(root->right);
+
+        }
+    }
+
+    return lh > rh ? lh+1 : rh+1;
+}
+

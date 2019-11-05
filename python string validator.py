91df0614046f4769affd745ef3c5bf72a0f13ccc
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2019-11-06 00:21:44 +0530

    Create python string validator.py

---
+++ python string validator.py
@@ -0,0 +1,7 @@
+S = raw_input()
+print any(c.isalnum() for c in S)
+print any(c.isalpha() for c in S)
+print any(c.isdigit() for c in S)
+print any(c.islower() for c in S)
+print any(c.isupper() for c in S)
+

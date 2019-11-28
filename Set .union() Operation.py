19788e48ac49ce0d1ec4bd6fb3ead6f185c863c8
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2019-11-28 18:16:35 +0530

    Create Set .union() Operation.py

---
+++ Set .union() Operation.py
@@ -0,0 +1,9 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+n1 = int(input())
+s1 = set((input()).split(" "))
+n2 = int(input())
+s2 = set((input()).split(" "))
+
+s3 = s2.union(s1)
+print(len(s3))

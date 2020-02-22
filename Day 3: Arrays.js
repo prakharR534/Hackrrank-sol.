70ab4bf88b0b82bc4223214df2821fc59091cd9b
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-02-22 19:44:31 +0530

    Create Day 3: Arrays.js

---
+++ Day 3: Arrays.js
@@ -0,0 +1,40 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    // Complete the function
+
+    var sort_a = nums.sort(function(a,b){ return a-b;});
+    var array = sort_a.filter(function(element,index,self){
+        return index ==self.indexOf(element);
+    })
+
+    return array[array.length -2];
+}
+

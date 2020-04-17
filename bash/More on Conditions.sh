5985bb397b58e314cd2d8cf845d4d90a86939075
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-04-17 19:56:22 +0530

    Create More on Conditions.sh

---
+++ bash/More on Conditions.sh
@@ -0,0 +1,14 @@
+read x
+read y 
+read z 
+
+if [[ "$x" == "$y" && "$x" == "$z" ]]; then
+echo "EQUILATERAL"
+
+elif [[ "$x" == "$y" || "$x" == "$z" || "$y" == "$z" ]]; then
+echo "ISOSCELES"
+
+else
+ echo "SCALENE"
+
+ fi

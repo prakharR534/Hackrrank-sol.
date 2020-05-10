3ff6152d7a19ba95fbde38f7e2c66ebf24e805b8
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-05-10 22:02:56 +0530

    Create Viral Advertising.cpp

---
+++ Viral Advertising.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main(){
+    int n,s=5,sum=0;
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+        sum = sum + floor(s/2);
+        s= floor(s/2)*3;
+    }
+    cout<<sum<<endl;
+    return 0;
+}

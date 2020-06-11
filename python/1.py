104ab1628a59a75d459cc0bb05ab1ab5ff23c474
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-06-11 15:07:27 +0530

    Create 1.py

---
+++ python/1.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# write your code here
+def avg(*args):
+    n = len(args)
+    av= sum(args) /n
+    return av
+    
+
+if __name__ == '__main__':

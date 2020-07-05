cf22831c589f0c60a38f4d3acf9b3a7efa6c921f
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-07-05 23:41:06 +0530

    Update Day: 3 throw.js

--- Day: 3 throw.js
+++ Day: 3 throw.js
@@ -15,6 +15,7 @@ process.stdin.on('end', _ => {
         return string.trim();
     });
     
+
     main();    
 });
 
@@ -37,6 +38,7 @@ return "YES";
     else if(a == 0){
 
 throw new Error("Zero Error");
+
     }
 
     else if (a < 0){

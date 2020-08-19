8490838d937488b5cb699aa2af1bea0d7d26fc0e
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-08-19 14:14:53 +0530

    Create Cats and a Mouse.cpp

---
+++ Cats and a Mouse.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,x;
+        cin>>a>>b>>x;
+
+        int d1 = abs(x-a);
+        int d2 = abs(x-b);
+
+        if(d1>d2) cout<<"Cat B\n";
+        else if(d2>d1) cout<<"Cat A\n";
+        else cout<<"Mouse C\n";
+    }
+   
+}

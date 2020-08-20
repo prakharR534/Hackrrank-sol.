78100b6c1dd37e9f54b939d0cadf6127660de456
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-08-20 15:17:27 +0530

    Create Beautiful Days at the Movies.cpp

---
+++ Beautiful Days at the Movies.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int rev(int i){
+    int rem=0,rev=0;
+
+    while(i){
+        rem = i % 10;
+        rev = rev*10 + rem;
+        i /= 10;
+    }
+    return rev;
+}
+
+int main()
+{
+    int s,e,k,c=0;
+    cin>>s>>e>>k;
+
+    for(int i=s;i<e+1;i++){
+        if(abs(i - rev(i)) % k == 0 )
+        c++;
+    }
+
+    cout<<c;
+
+
+    return 0;
+}
+   

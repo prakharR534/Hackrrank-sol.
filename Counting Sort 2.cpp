53cbdd8c8a19ef7bebfa0f8cf1d5499ea27811b9
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-09-05 23:50:07 +0530

    Create Counting Sort 2.cpp

---
+++ Counting Sort 2.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+    int n;cin>>n;
+    int A[n];
+    int count[100]={0};
+    for(int i=0;i<n;i++){
+        cin>>A[i];
+        count[A[i]]++;        
+    }
+
+    for(int i=0;i<100;i++){
+      for(int j=1;j<=count[i];j++){
+          cout<<i<<" ";
+      }       
+    }
+
+}

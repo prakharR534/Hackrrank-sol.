1579f377101ac2f2acfad2fd5a628d8156d86af4
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-09-19 14:33:43 +0530

    Create Halloween party.cpp

---
+++ Mathematics/Halloween party.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    long long int t;
+    cin>>t;
+    while(t--){
+        long long int n;
+        cin>>n;
+
+        cout<<(n/2) * (n-n/2)<<endl;
+    }
+    return 0;
+}

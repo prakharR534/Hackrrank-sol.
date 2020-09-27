bf4244a4fbcd4c99e19b10a2d9afc0a626ccfdf3
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-09-27 23:55:59 +0530

    Update 2D Array - DS.cpp

--- 2D Array - DS.cpp
+++ 2D Array - DS.cpp
@@ -9,6 +9,7 @@ int sum = -1000;
 for(int i=0;i<4;i++){
     for(int j=0;j<4;j++){
         int top = arr[i][j]+arr[i][j+1]+arr[i][j+2];
+        
         int middle =arr[i+1][j+1];
         int bottom = arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2];
 
